8218e4b75962b03a1ec1b1b06fb8b24310a8e4ef
shahinurdev <mdshahinur6@gmail.com>
2021-06-05 11:36:31 +0600

    skills reset

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -3,30 +3,45 @@ import './Skills.css'
 
 const Skills = () => {
     return (
-        <section  className="allSection">
+        <section className="allSection">
         <div className="skills">
           <h1>Skills</h1>
           <div className="content">
             <div>
-              <strong>Languages:</strong>
-              <li>CSS3</li>
-              <li>HTML5</li>
-              <li>Node.js</li>
+              <strong>Comfortable:</strong>
               <li>JavaScript</li>
+              <li>ES6</li>
+              <li>React Router</li>
+              <li>HTML</li>
+              <li> CSS</li>
+              <li>SASS</li>
+              <li>Bootstrap</li>
             </div>
             <div>
-              <strong>Libraries:</strong>
-              <li>Bootstrap</li>
+              <strong>Familiar:</strong>
+              <li> TypeScript</li>
+              <li>Node.js</li>
+              <li>Redux</li>
               <li>Express.js</li>
-              <li>Material-ui</li>
-              <li>React.js</li>
+              <li>MongoDB</li>
+              <li>Mongoose</li>
+              <li>Material-UI</li>
+              <li>React Bootstrap</li>
+              <li>Wordpress</li>
+              <li>React Native</li>
             </div>
             <div>
               <strong>Tools:</strong>
-              <li>Eslint</li>
-              <li>Webpack</li>
-              <li>Chrome Dev</li>
-              <li>Git & Github</li>
+              <li>Git</li>
+              <li>Github</li>
+              <li>VS Code</li>
+              <li>JEST</li>
+              <li>Chrome Dev Tools</li>
+              <li>Heroku</li>
+              <li>Netlify</li>
+              <li>Firebase</li>
+              <li> Jira</li>
+              <li>webpack</li>
             </div>
             {/* <div>
               <strong>Uint Test:</strong>
